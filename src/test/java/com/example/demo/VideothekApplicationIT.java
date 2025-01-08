c5b53cc2ba39f1834306466dcbc3ab78cad598ff
Laurin Krüger <krueger@th-brandenburg.de>
2025-01-08 16:44:54 +0100

    Add Tests

--- src/test/java/com/example/demo/VideothekApplicationIT.java
+++ src/test/java/com/example/demo/VideothekApplicationIT.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VideothekApplicationTests {
+class VideothekApplicationIT {
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void my_simple_unit_test() {
-		System.out.println("This is a unit test!");
+	void my_simple_integration_test() {
+		System.out.println("This is a integration test!");
 	}
 }
