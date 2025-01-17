9b6e4236558b008c115a2f9869ab408df181f719
Saftbear <bastiderspasti.irmer.ich@gmail.com>
2025-01-17 14:03:43 +0100

    fix: - unused imports pmd error message

--- src/main/java/com/example/demo/service/S3Service.java
+++ src/main/java/com/example/demo/service/S3Service.java
@@ -6,15 +6,13 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
