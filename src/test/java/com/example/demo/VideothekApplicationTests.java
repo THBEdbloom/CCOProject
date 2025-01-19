03ca18ba2d0765b58cb7b1b93c6fd4fd085eeebc
THBEdbloom <91316363+THBEdbloom@users.noreply.github.com>
2025-01-19 16:04:56 +0100

    Update VideothekApplicationTests.java

--- src/test/java/com/example/demo/VideothekApplicationTests.java
+++ src/test/java/com/example/demo/VideothekApplicationTests.java
@@ -1,48 +1,27 @@
-// Unit Tests
 package com.example.demo;
 
-import com.example.demo.entity.Film;
-import com.example.demo.entity.Playlist;
-import com.example.demo.repository.FilmRepository;
-import com.example.demo.repository.PlaylistRepository;
-import com.example.demo.service.VideothekService;
+import com.example.demo.controller.VideothekController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import com.example.demo.controller.VideothekController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 class VideothekApplicationTests {
-
-    @Mock
-    private FilmRepository filmRepository;
-
-    @Mock
-    private PlaylistRepository playlistRepository;
-
-    @InjectMocks
-    private VideothekService videothekService;
 
     @Autowired
     private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        // Hier ist keine spezielle Initialisierung nötig, MockMvc wird durch AutoConfigureMockMvc bereitgestellt
     }
 
     @Test
